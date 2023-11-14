0a36bc0298f533be60fd25fed7458fff031009a4
kasiansky <happyhusband108gmail.com>
2023-11-14 10:44:35 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,13 @@ import { Product } from './modules/products/product.entity';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'postgres',
-      host: 'localhost',
+      host: 'dpg-cl9hgmdo7jlc73fdst6g-a.oregon-postgres.render.com',
       port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'shopping_list',
+      username: 'instance_shopping_list_user',
+      password: 'hhx43WjA5QukHZ8RcH4deHH9xuXeetxg',
+      database: 'instance_shopping_list',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
+      synchronize: false, // Set to false in production
       
     }),TypeOrmModule.forFeature([Product, Category]),
     ProductModule,
